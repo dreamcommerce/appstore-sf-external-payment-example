ee796e76f049311bb6a6125a5e73355fcfaf0577
Jakub Grzesiak <jakub.grzesiak@jg-webtech.pl>
2025-07-08 12:10:29 +0200

    Introduce payment configuration feature with support for CRUD operations and async handling via Symfony Messenger

--- app/src/Controller/PaymentDetailsController.php
+++ app/src/Controller/PaymentDetailsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\Payment\PaymentServiceInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,26 +11,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PaymentDetailsController extends AbstractController
 {
+    private LoggerInterface $logger;
     private PaymentServiceInterface $paymentService;
 
-    public function __construct(PaymentServiceInterface $paymentService)
+    public function __construct(LoggerInterface $logger, PaymentServiceInterface $paymentService)
     {
+        $this->logger = $logger;
         $this->paymentService = $paymentService;
     }
 
-    #[Route('/app-store/view/payment-details', name: 'payment_details', methods: ['GET'])]
-    public function paymentDetailsAction(Request $request): Response
+    #[Route('/app-store/view/payment-details/{paymentId}', name: 'payment_details', methods: ['GET'])]
+    public function paymentDetailsAction(Request $request, $paymentId): Response
     {
-        $paymentId = $request->query->get('payment-id');
         $shopCode = $request->query->get('shop');
+        $paymentId = $request->query->get('payment', 61);
         $locale = $request->query->get('translations', 'pl_PL');
-        $payment = null;
 
         if ($shopCode && $paymentId) {
             $payment = $this->paymentService->getPaymentById($shopCode, $paymentId, $locale);
         }
         return $this->render('payment-details.twig', [
             'payment' => $payment,
+            'paymentId' => $paymentId,
+            'shopCode' => $shopCode,
             'locale' => $locale
         ]);
     }
